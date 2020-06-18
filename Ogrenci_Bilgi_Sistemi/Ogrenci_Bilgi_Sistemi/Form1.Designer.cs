cf73c7abd70bc38cdcc63114a730cf21b78bd3dc
Hasan6606 <62176480+Hasan6606@users.noreply.github.com>
2020-06-18 15:45:37 +0300

    I added  a database file which is called "Ogrenci_Kayit.accdb"

---
+++ Ogrenci_Bilgi_Sistemi/Ogrenci_Bilgi_Sistemi/Form1.Designer.cs
@@ -0,0 +1,221 @@
+﻿namespace Ogrenci_Bilgi_Sistemi
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.textBox5 = new System.Windows.Forms.TextBox();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.Güncelle = new System.Windows.Forms.Button();
+            this.Sil = new System.Windows.Forms.Button();
+            this.Ekle = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 19);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(44, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "TC_No:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 45);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(56, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Ad-Soyad:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(12, 72);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(73, 13);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Doğum Tarihi:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(12, 103);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(63, 13);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "Telefon No:";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(12, 132);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(36, 13);
+            this.label5.TabIndex = 4;
+            this.label5.Text = "Bölüm";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(93, 19);
+            this.textBox1.MaxLength = 11;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 20);
+            this.textBox1.TabIndex = 8;
+            this.textBox1.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.textBox1_KeyPress);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(93, 45);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(100, 20);
+            this.textBox2.TabIndex = 9;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(93, 72);
+            this.textBox3.MaxLength = 6;
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(100, 20);
+            this.textBox3.TabIndex = 10;
+            this.textBox3.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.textBox3_KeyPress);
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(93, 103);
+            this.textBox4.MaxLength = 11;
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(100, 20);
+            this.textBox4.TabIndex = 11;
+            this.textBox4.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.textBox4_KeyPress);
+            // 
+            // textBox5
+            // 
+            this.textBox5.Location = new System.Drawing.Point(93, 132);
+            this.textBox5.Name = "textBox5";
+            this.textBox5.Size = new System.Drawing.Size(100, 20);
+            this.textBox5.TabIndex = 12;
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(225, 12);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(548, 193);
+            this.dataGridView1.TabIndex = 19;
+            this.dataGridView1.CellDoubleClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellDoubleClick);
+            // 
+            // Güncelle
+            // 
+            this.Güncelle.Image = global::Ogrenci_Bilgi_Sistemi.Properties.Resources.Actions_edit_clear_history_icon;
+            this.Güncelle.Location = new System.Drawing.Point(321, 213);
+            this.Güncelle.Name = "Güncelle";
+            this.Güncelle.Size = new System.Drawing.Size(105, 120);
+            this.Güncelle.TabIndex = 18;
+            this.Güncelle.UseVisualStyleBackColor = true;
+            this.Güncelle.Click += new System.EventHandler(this.Güncelle_Click);
+            // 
+            // Sil
+            // 
+            this.Sil.Image = global::Ogrenci_Bilgi_Sistemi.Properties.Resources.Editing_Delete_icon;
+            this.Sil.Location = new System.Drawing.Point(178, 213);
+            this.Sil.Name = "Sil";
+            this.Sil.Size = new System.Drawing.Size(105, 120);
+            this.Sil.TabIndex = 17;
+            this.Sil.UseVisualStyleBackColor = true;
+            this.Sil.Click += new System.EventHandler(this.Sil_Click);
+            // 
+            // Ekle
+            // 
+            this.Ekle.Image = global::Ogrenci_Bilgi_Sistemi.Properties.Resources.Programming_Add_Property_icon;
+            this.Ekle.Location = new System.Drawing.Point(12, 213);
+            this.Ekle.Name = "Ekle";
+            this.Ekle.Size = new System.Drawing.Size(141, 120);
+            this.Ekle.TabIndex = 16;
+            this.Ekle.UseVisualStyleBackColor = true;
+            this.Ekle.Click += new System.EventHandler(this.Ekle_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(804, 354);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.Güncelle);
+            this.Controls.Add(this.Sil);
+            this.Controls.Add(this.Ekle);
+            this.Controls.Add(this.textBox5);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "ÖĞRENCİ BİLGİ SİSTEMİ";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.TextBox textBox5;
+        private System.Windows.Forms.Button Ekle;
+        private System.Windows.Forms.Button Sil;
+        private System.Windows.Forms.Button Güncelle;
+        private System.Windows.Forms.DataGridView dataGridView1;
+    }
+}
+
---
+++ Ogrenci_Bilgi_Sistemi/Ogrenci_Bilgi_Sistemi/Form1.cs
@@ -0,0 +1,208 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.OleDb;//Bu satırda veritabanıyla ilgili işlem için eklememiz gereken kütüphane satırıdır.
+
+/*   Altt görmüş oldüğunuz program bilgisayar programlama-II adlı dersin proje ödevidir.Burda form uygulaması
+ *tabanında veritabanı(Microsoft Access) ilişkisel olarak anlık kullanıcıdan alınan verilerin bazı koşul
+ * ve şartlar ile işlemler gerçekleşmiştir.
+ * Umarım alttaki kod yapısının size katkısı olmuştur.Eğer kod yapımda hata bulursanız lütfen doğrusuyla beraber
+ * beni bilgilendirirseniz sevinirim.
+ * İyi Çalışmalar.
+ * Hasan Hüseyin Aygar
+ * 18.06.2020
+ */
+namespace Ogrenci_Bilgi_Sistemi
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        //Form kısmındaki butonlara eklemiş olduğum görseller "Properties" kısmından ekledim.
+        //Formun üstteki iconunu  "Properties" kısmından ekledim.Eklerken ekliyeceğiniz görselin ".ico" uzantılı olmalıdır.Başka çözüm yolu muhakkak vardır ama ben ".ico" uzantılı görseli eklemeyi tercih ettim.
+        //alttaki değişkenler global(genel) olarak tanımlanmış ki her buton tıklama işlemi için ayrı ayrı tekrardan tanımlamamak için.
+        OleDbConnection baglanti = new OleDbConnection("Provider = Microsoft.ACE.OLEDB.12.0; Data Source = C:\\Users\\hasan\\Documents\\Ogrenci_Kayit.accdb");
+        //Lütfen siz blgisayarınıza indirdiğiniz zaman  Microsoft Access olduğunu kontrol ediniz ve veritabanı dosyası nerde ise konumunu düzeltiniz.
+        //Üstteki kod yapısında ilişki kuracağımız veri tabanının konumu belirleyip "baglanti" adında değişkene atadık.
+        OleDbCommand komut; //Bu tanımlamada ise sql kod yapısını kullanmak için tanımladık.
+        OleDbDataAdapter adtr; //Burda ise kullanıcıdan aldığımız işlem ve verileri dataset aracılığı ile veritabanına aktarmak için tanımladık.
+        DataTable table = new DataTable();//Bu satırda ise kullanıcıdan alınıcak bilgileri veri tabanını taşırkenki aracı tablo olarak tanımladık.
+
+        private void Ekle_Click(object sender, EventArgs e)
+        {
+            //Alt alta görmüş olduğunuz if else yapısında ise bazı alınması şart olan bilgileri kullanıcı daha veritabanına kontrol ettirmeden biz kontrol edip duruma göre bilgilendiriyoruz ve gereken müdahele dataset kısmına geçmeden kod yapsındaki şarta bağlı olarak nasıl isteniyorsa öyle bir veri girişi oluyor.
+            if (textBox1.TextLength < 11)
+            {
+                MessageBox.Show("T.C. kimlik numarası 11 haneli olur.Lütfen 11 haneli sayısal veri giriniz.", "Uyarı!");
+                textBox1.Text = "";
+            }
+            else
+            {
+                if (textBox3.TextLength < 6)
+                {
+                    MessageBox.Show("Lütfen 6 haneli (gün-ay-yıl örn:160520)olarak giriniz.", "Uyarı!");
+                    textBox3.Text = "";
+                }
+                else {
+                    if (textBox4.TextLength < 11)
+                    {
+                        MessageBox.Show("Telefon numarası 11 hanelidir(Örn:05554443322).", "Uyarı!");
+                        textBox4.Text = "";
+                    }
+                    else {
+
+
+                        baglanti.Open();//Bu satırda ise tanımladığımız adresteki veritabanını açtık.
+                        komut = new OleDbCommand("INSERT INTO Kisisel_Bilgiler (TCKimlik,İsimSoyisim,DogumTarihi,TelefonNo,Bölüm) VALUES('" + textBox1.Text + "','" + textBox2.Text + "','" + textBox3.Text + "','" + textBox4.Text + "','" + textBox5.Text + "')", baglanti);
+                        //Üstteki kod yapısından "oleDbCommand()" yapsının içineki sql kodu ile ekleme işlemi yazıp textboxdaki verileri ilişkisel olarak veritabanı sütunlarına eşleştirdik.
+                        komut.ExecuteNonQuery();//Bu satırda ise üstteki "komut" adlı değişkendeki sql kod yapısını "ExecuteNonQuery()" sayesinde çalıştırmak için.
+                        baglanti.Close();//Bu satırda ise  "baglanti" adlı değişkende açık olan veritabanını kapattık. 
+                        listele();//Bu satırda ise "listele()" sayesinde anlık değişikleri listeledik.
+                        MessageBox.Show("Tebrikler ekleme işlemi başarıyla tamamladınız.", "Ekleme");
+                        for (int a = 0; a < Controls.Count; a++) //Burdaki döngüde ise textbox ların içini temizlemek içindir.
+                        {
+                            if (Controls[a] is TextBox)
+                            {
+
+                                Controls[a].Text = "";
+                            }
+
+
+                        }
+                    }
+                }
+
+
+            } 
+
+
+        }
+
+
+
+
+
+     
+
+
+
+
+
+
+
+private void listele() {
+
+            table.Clear();//Burda ise "table" yapısını temizlemiş olduk çünkü sürekli aynı verileri alt alta yazdırmamak için.
+            baglanti.Open();//Bu satırda ise tanımladığımız adresteki veritabanını açtık.
+            komut = new OleDbCommand("select * from Kisisel_Bilgiler", baglanti);
+            //Üstteki kod yapısından "oleDbCommand()" yapısının içindeki sql kodu ile baglanti adlı veritabanında "Kisisel_Bilgiler" adlı tablonun tüm kayıtlarını göstermiş olduk.
+            adtr.Fill(table); //Burda ise dataset aracılığı ile gösterilicek tabloyu "table" adlı aracı tabloya atadık.
+            dataGridView1.DataSource = table; //Tabloyu göstereceğimiz  "dataGridView1" yapının içine "table" daki tabloyu aktardık.
+            baglanti.Close();//Bu satırda ise  "baglanti" adlı değişkende açık olan veritabanını kapattık.
+
+
+        }
+
+        private void Sil_Click(object sender, EventArgs e)
+        {
+            baglanti.Open();//Bu satırda ise tanımladığımız adresteki veritabanını açtık.
+            komut = new OleDbCommand("DELETE * FROM Kisisel_Bilgiler WHERE TCKimlik='" + dataGridView1.CurrentRow.Cells[0].Value.ToString() + "'", baglanti);
+            //Üstteki kod yapısından "oleDbCommand()" yapısının içindeki sql kodu ile "dataGridView1" yapısında seçili olan verinin  "baglanti" yapısındaki veritabanında "Kisisel_Bilgiler" adlı tablosundaki tüm kayıtlarını silmiş olduk.
+            komut.ExecuteNonQuery();//Bu satırda ise üstteki "komut" adlı değişkendeki sql kod yapısını "ExecuteNonQuery()" sayesinde çalıştırmak için.
+            baglanti.Close();//Bu satırda ise  "baglanti" adlı değişkende açık olan veritabanını kapattık.
+            listele();//Bu satırda ise "listele()" sayesinde anlık değişikleri listeledik.
+            MessageBox.Show("Silme işlemi başarlıyla tamamladınız.","Silme");
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            listele();//Form yüklenirken "listele()" yapısıyla ekrana tabloyu listelenmiştir.
+        }
+
+        private void dataGridView1_CellDoubleClick(object sender, DataGridViewCellEventArgs e)
+        {
+            //Alltaki kod yapısıyla "dataGridView1" işaretli veriye çift tıklayarak ilişkisel olarak textbox lara yazdırdık.
+            textBox1.Text = dataGridView1.CurrentRow.Cells["TCKimlik"].Value.ToString();
+            textBox2.Text = dataGridView1.CurrentRow.Cells["İsimSoyisim"].Value.ToString();
+            textBox3.Text = dataGridView1.CurrentRow.Cells["DogumTarihi"].Value.ToString();
+            textBox4.Text = dataGridView1.CurrentRow.Cells["TelefonNo"].Value.ToString();
+            textBox5.Text = dataGridView1.CurrentRow.Cells["Bölüm"].Value.ToString();
+        }
+
+        private void Güncelle_Click(object sender, EventArgs e)
+        {
+            //Alt alta görmüş olduğunuz if else yapısında ise bazı alınması şart olan bilgileri kullanıcı daha veritabanına kontrol ettirmeden biz kontrol edip duruma göre bilgilendiriyoruz ve gereken müdahele dataset kısmına geçmeden kod yapsındaki şarta bağlı olarak nasıl isteniyorsa öyle bir veri girişi oluyor.
+            if (textBox1.TextLength < 11)
+            {
+                MessageBox.Show("T.C. kimlik numarası 11 haneli olur.Lütfen 11 haneli sayısal veri giriniz.", "Uyarı!");
+                textBox1.Text = "";
+            }
+            else
+            {
+                if (textBox3.TextLength < 6)
+                {
+                    MessageBox.Show("Lütfen 6 haneli (gün-ay-yıl örn:160520)olarak giriniz.", "Uyarı!");
+                    textBox3.Text = "";
+                }
+                else
+                {
+                    if (textBox4.TextLength < 11)
+                    {
+                        MessageBox.Show("Telefon numarası 11 hanelidir(Örn:05554443322).", "Uyarı!");
+                        textBox4.Text = "";
+                    }
+                    else
+                    {
+
+
+                        baglanti.Open(); //Bu satırda ise tanımladığımız adresteki veritabanını açtık.
+                        komut = new OleDbCommand("UPDATE Kisisel_Bilgiler SET İsimSoyisim='" + textBox2.Text + "',DogumTarihi='" + textBox3.Text + "',TelefonNo='" + textBox4.Text + "',Bölüm='" + textBox5.Text + "' where TCKimlik='" + textBox1.Text + "'", baglanti);
+                        //Üstteki kod yapısından "oleDbCommand()" yapısının içindeki sql kodu ile "dataGridView1" yapısında seçili olan verinin  "baglanti" yapısındaki veritabanında "Kisisel_Bilgiler" adlı tablosundaki tüm kayıtlarını "TCKimlik" kısmını şartlarndırarak güncellemiş olduk.
+                        //T.C. kimlik no güncellenemeyen bir veridir.Veritabanında primary key(birincil anahtar) olarak tanımlıdır.
+                        komut.ExecuteNonQuery();//Bu satırda ise üstteki "komut" adlı değişkendeki sql kod yapısını "ExecuteNonQuery()" sayesinde çalıştırmak için.
+                         baglanti.Close();//Bu satırda ise  "baglanti" adlı değişkende açık olan veritabanını kapattık.
+                        listele();//Bu satırda ise "listele()" sayesinde anlık değişikleri listeledik.
+                        MessageBox.Show("Tebrikler güncelleme işlemi başarıyla tamamladınız.", "Güncelleme");
+                        for (int a = 0; a < Controls.Count; a++)
+                        {
+                            if (Controls[a] is TextBox)
+                            {
+
+                                Controls[a].Text = "";
+                            }
+
+
+                        }
+
+                    }
+                }
+
+
+            }
+        }
+        //Alttaki kodlarda ise belirli textboxlarda(T.C.KİMLİK,TELEFON VE DOĞUM TARİHİ) veri girişlerinde sadece sayı girmesini sağladık.
+        //Klavyeden girerken sağ tarafınızdaki sayılar basmanıza rağmen sayı girişi olmuyorsa sayı tuşlarınızın çalıştınızı kontrol ediniz.Eğer tuşlar çalışıyorsa lütfen bilgisayarınızdaki "num lock" tuşunuzun aktifliğini kontrol ediniz.
+        private void textBox1_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            e.Handled = !char.IsDigit(e.KeyChar) && !char.IsControl(e.KeyChar);
+        }
+
+        private void textBox3_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            e.Handled = !char.IsDigit(e.KeyChar) && !char.IsControl(e.KeyChar);
+        }
+
+        private void textBox4_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            e.Handled = !char.IsDigit(e.KeyChar) && !char.IsControl(e.KeyChar);
+        }
+    }
+}
